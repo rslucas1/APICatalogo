0f243387be333008e52defca74a8b52e60949412
Lucas Silva <rs.lucas1@gmail.com>
2022-02-23 08:09:04 -0300

    Projeto Iniciado

---
+++ APICatalogo/Domain/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Domain
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+
+         
+
+    }
+}
